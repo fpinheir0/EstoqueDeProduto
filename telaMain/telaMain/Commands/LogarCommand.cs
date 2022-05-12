23a1c3e7ba15dd6a7a452f319134f590874e842c
fepinheir0 <f.pinheir0@outlook.com>
2022-05-12 16:09:50 -0300

    no message

---
+++ telaMain/telaMain/Commands/LogarCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace telaMain.Commands
+{
+    public class LogarCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            string senha = (parameter as PasswordBox).Password;
+            MessageBox.Show("sua senha é: " + senha);
+        }
+    }
+}
---
+++ telaMain/telaMain/ViewModels/LogarViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using telaMain.Commands;
+
+namespace telaMain.ViewModels
+{
+    public class LogarViewModel
+    {
+        public LogarCommand Logar { get; set; }
+    
+        public LogarViewModel ()
+        {
+            Logar = new LogarCommand();
+        }
+    }
+}
